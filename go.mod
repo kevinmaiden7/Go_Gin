d0fe2b34004a05985f229c765d4036fa89950b1e
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-01-28 22:38:16 -0500

    Uploading files

---
+++ go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	rsc.io/quote v1.5.2
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "rsc.io/quote"
+import "github.com/gin-gonic/gin"
+import "net/http"
+
+func main(){
+	app := gin.Default()
+
+	app.GET("/", func(c *gin.Context){
+		c.String(http.StatusOK, quote.Hello())
+	})
+
+	app.GET("/index", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"message": "index OK",
+		})
+	})
+
+	app.GET("/user/:name", func(c *gin.Context){
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+	
+	app.Run() // by default listen and serve on 0.0.0.0:8080
+	// router.Run(":3000") for a hard coded port
+}
