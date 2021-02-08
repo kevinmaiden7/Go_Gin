5639329f4bb9f316d9381c4fdeef516d28922950
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-02-08 17:06:37 -0500

    adding go endpoint

--- main.go
+++ main.go
@@ -1,27 +1,33 @@
 package main
 
-import "rsc.io/quote"
-import "github.com/gin-gonic/gin"
-import "net/http"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"rsc.io/quote"
+)
 
-func main(){
+func main() {
 	app := gin.Default()
 
-	app.GET("/", func(c *gin.Context){
+	app.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, quote.Hello())
 	})
 
-	app.GET("/index", func(c *gin.Context){
+	app.GET("/go", func(c *gin.Context) {
+		c.String(http.StatusOK, quote.Go())
+	})
+
+	app.GET("/index", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "index OK",
 		})
 	})
 
-	app.GET("/user/:name", func(c *gin.Context){
+	app.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.String(http.StatusOK, "Hello %s", name)
 	})
-	
+
 	app.Run() // by default listen and serve on 0.0.0.0:8080
 	// app.Run(":3000") for a hard coded port
 }
